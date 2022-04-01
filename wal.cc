8b1fd72195aed9cbdace64115e3b4b0bd6aa116b
HuanyYang <eftcyhy@gmail.com>
2022-04-01 15:23:55 +0800

    feat(wal): support file creation, AddRecord and LoadLogToMem

--- wal.cc
+++ wal.cc
@@ -1,1 +1,43 @@
 #include "wal.h"
+#include <unistd.h>
+
+void splitKV(const std::string &str, std::string &key, std::string &value) {
+  if (str.empty() || str.find(delimiter) == std::string::npos) {
+    return;
+  }
+  key = str.substr(0, str.find(delimiter));
+  value = str.substr(str.find(delimiter) + 1, str.size());
+}
+
+// WAL不存在则创建
+WAL::WAL(const std::string &logPath) : logPath_(logPath) {
+  if (access(logPath_.c_str(), F_OK) != 0) {
+    logWriter_.open(logPath_);
+  }
+}
+
+WAL::~WAL() {
+  if (logWriter_.is_open())
+    logWriter_.close();
+  if (logReader_.is_open())
+    logReader_.close();
+}
+
+bool WAL::AddRecord(const std::string &record) {
+  logWriter_.open(logPath_, std::ios_base::app);
+  logWriter_ << record << "\n";
+  logWriter_.flush();
+  logWriter_.close();
+  return true;
+}
+
+bool WAL::LoadLogToMem(SkipList<std::string, std::string> *mem) {
+  logReader_.open(logPath_);
+  std::string line, key, value;
+  while (getline(logReader_, line)) {
+    splitKV(line, key, value);
+    mem->insertElement(key, value);
+  }
+  logReader_.close();
+  return true;
+}
--- wal.h
+++ wal.h
@@ -1,6 +1,29 @@
 #ifndef RAFT_KV_WAL_H
 #define RAFT_KV_WAL_H
 
-class WAL {};
+#include "SkipList.h"
+#include "fstream"
+
+const char delimiter = ':';
+
+class WAL {
+public:
+  explicit WAL(const std::string &logPath);
+
+  WAL(const WAL &) = delete;
+  WAL &operator=(const WAL &) = delete;
+
+  ~WAL();
+
+  bool AddRecord(const std::string &record);
+
+  // 将WAL中的内容恢复到memtable
+  bool LoadLogToMem(memtable *mem);
+
+private:
+  std::string logPath_;
+  std::ofstream logWriter_;
+  std::ifstream logReader_;
+};
 
 #endif // RAFT_KV_WAL_H
